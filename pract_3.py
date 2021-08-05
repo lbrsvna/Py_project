f47d10cb227ef97e487988c4f1e6478451860d15
lbrsvna <lbrsvna@gmail.com>
2021-08-05 21:27:48 +0300

    pract 3

--- pract_3.py
+++ pract_3.py
@@ -1,10 +1,22 @@
-# Узнайте у пользователя число n.
-# Найдите сумму чисел n + nn + nnn.
-# Например, пользователь ввёл число 3.
-# Считаем 3 + 33 + 333 = 369
+# Пользователь вводит месяц в виде целого числа от 1 до 12.
+# Сообщить, к какому времени года относится месяц (зима, весна, лето, осень).
+# Напишите решения через list и dict.
 
-number= int(input("Введите любое число:"))
-number_2 = int(str(number) + str(number))
-number_3 = int(str(number) + str(number) + str(number))
-sum = int(number + number_2 + number_3)
-print(f'Сумма чисел равна: {sum}')
+seasons_list = ['зима', 'весна', 'лето', 'осень']
+seasons_dict = {0 : 'зима', 1 : 'весна', 2 : 'лето', 3 : 'осень'}
+number = int(input("Введите номер месяца: "))
+if number ==1 or number == 12 or number == 2:
+        print(seasons_dict.get(0))
+        print(seasons_list[0])
+elif number == 3 or number == 4 or number ==5:
+    print(seasons_dict.get(1))
+    print(seasons_list[1])
+elif number == 6 or number == 7 or number == 8:
+    print(seasons_dict.get(2))
+    print(seasons_list[2])
+
+elif month == 9 or month == 10 or month == 11:
+    print(seasons_dict.get(3))
+    print(seasons_list[3])
+else:
+        print("Введено неверное значение")
